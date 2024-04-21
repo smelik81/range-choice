0e1f9bef0e1faa07f7d8633f09d036455ebd579f
smelik81 <artem.smelyanskiy81@gmail.com>
2024-04-21 22:48:15 +0300

    finished first part

--- js/main.js
+++ js/main.js
@@ -1,55 +1,55 @@
 window.addEventListener('load', function () {
 
     const userArray = [
-      {
-        id: 1,
-        name: "Erling Haaland",
-        age: 22,
-        photo: "img/holland.jpg",
-        club: "Manchester Citi",
-        position: "Forward",
-        price: 100,
-      },
+        {
+            id: 1,
+            name: "Erling Haaland",
+            age: 22,
+            photo: "img/holland.jpg",
+            club: "Manchester Citi",
+            position: "Forward",
+            price: 100,
+        },
 
-      {
-        id: 2,
-        name: "Kylian Mbappé",
-        age: 24,
-        photo: "img/mbape.jpg",
-        club: "FC Paris Saint-Germain",
-        position: "Forward",
-        price: 100,
-      },
+        {
+            id: 2,
+            name: "Kylian Mbappé",
+            age: 24,
+            photo: "img/mbape.jpg",
+            club: "FC Paris Saint-Germain",
+            position: "Forward",
+            price: 100,
+        },
 
-      {
-        id: 3,
-        name: "Vinicius Junior",
-        age: 23,
-        photo: "img/vinicius.jpg",
-        club: "Real Madrid",
-        position: "Forward",
-        price: 95,
-      },
+        {
+            id: 3,
+            name: "Vinicius Junior",
+            age: 23,
+            photo: "img/vinicius.jpg",
+            club: "Real Madrid",
+            position: "Forward",
+            price: 95,
+        },
 
-      {
-        id: 4,
-        name: "Bukayo Saka",
-        age: 21,
-        photo: "img/saka.jpg",
-        club: "FC Arsenal",
-        position: "Forward",
-        price: 92,
-      },
+        {
+            id: 4,
+            name: "Bukayo Saka",
+            age: 21,
+            photo: "img/saka.jpg",
+            club: "FC Arsenal",
+            position: "Forward",
+            price: 92,
+        },
 
-      {
-        id: 5,
-        name: "Victor Osimhen",
-        age: 24,
-        photo: "img/osimhen.jpg",
-        club: "Napoli",
-        position: "Defender",
-        price: 90,
-      },
+        {
+            id: 5,
+            name: "Victor Osimhen",
+            age: 24,
+            photo: "img/osimhen.jpg",
+            club: "Napoli",
+            position: "Defender",
+            price: 90,
+        },
 
         {
             id: 6,
@@ -191,23 +191,24 @@ window.addEventListener('load', function () {
             price: 18,
         },
  
-      {
-        id: 20,
-        name: "Lautaro Martínez",
-        age: 25,
-        photo: "img/martinez.jpg",
-        club: "Inter Milan",
-        position: "Forward",
-        price: 16,
-      }, 
+        {
+            id: 20,
+            name: "Lautaro Martínez",
+            age: 25,
+            photo: "img/martinez.jpg",
+            club: "Inter Milan",
+            position: "Forward",
+            price: 16,
+        },
     ];
 
     const container = document.querySelector(".users-container");
     const nameFilter = document.querySelector("#nameFilter");
     const priceFilter = document.querySelector("#priceFilter");
-    const positionFilter = document.querySelectorAll("input[type='radio']");
-    container.insertAdjacentHTML('beforeend', createMarkup(userArray));
+    const positionFilter = document.querySelector(".js-filter");
 
+
+    container.insertAdjacentHTML('beforeend', createMarkup(userArray));
     function createMarkup(arr) {
         return arr
             .map(person =>
@@ -226,33 +227,66 @@ window.addEventListener('load', function () {
 
     }
 
-   nameFilter.addEventListener("input", (event) => {
-     const filteredUsersName = userArray.filter((user) =>
-       user.name.toLowerCase().includes(nameFilter.value.toLowerCase())
-     );
-     container.innerHTML = createMarkup(filteredUsersName);
-   });
+    nameFilter.addEventListener("input", (event) => {
+        const filteredUsersName = userArray.filter((user) =>
+            user.name.toLowerCase().includes(nameFilter.value.toLowerCase())
+        );
+        container.innerHTML = createMarkup(filteredUsersName);
+    });
     
     priceFilter.addEventListener("input", (event) => {
-        console.log(Number(event.target.value));
         const filteredUsersPrice = userArray.filter(
-          (user) => user.price >= Number(event.target.value)
+            (user) => user.price >= Number(event.target.value)
         );
         container.innerHTML = createMarkup(filteredUsersPrice);
-    })
-
-        
-    positionFilter.forEach((filter) => {
-      filter.addEventListener("change", () => {
-        const filteredPosition = filter.value;
-        const filteredUsersPosition = userArray.filter((user) => {
-          return (
-            filteredPosition === "all" || user.position === filteredPosition
-          );
-        });
+    });
+    
+    positionFilter.addEventListener("change", (event) => {
+        const filteredPosition = event.target.value.toLowerCase();
+        if (filteredPosition === "all") {
+            return container.innerHTML = createMarkup(userArray);
+        };
+        const filteredUsersPosition = userArray.filter(
+            (user) =>
+                user.position.toLocaleLowerCase() === filteredPosition);
         container.innerHTML = createMarkup(filteredUsersPosition);
-      });
-    });  
+    });
+
+    /* priceFilter.addEventListener("input", (event) => {
+        state.price = Number(event.target.value);
+        updateView();
+    });
+
+    positionFilter.addEventListener('change', (event) => {
+        state.position = event.target.value.toLowerCase();
+        if (state.position === "all") {
+          return (container.innerHTML = createMarkup(userArray));
+        } else {
+            
+        }
+        updateView();
+   })
+    
+     const state = {
+       position: "all",
+       price: 100,
+     };
+
+     function filterData() {
+       return userArray.filter((user) => {
+         const setName = user.price <= state.price;
+         const setPosition =
+           state.position === "all" || user.position === state.position;
+         return setName && setPosition;
+       });
+     }
+
+    function updateView() {
+      const filteredData = filterData();
+      container.innerHTML = createMarkup(filteredData);
+    } */
     
 
 });
+
+
